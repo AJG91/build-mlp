f208ada8a365fa843c392e3d880f95726e731fb7
Alberto J Garcia <albertojg1991@gmail.com>
2025-09-19 23:07:03 -0700

    Class that builds MLP model

---
+++ build_mlp/model.py
@@ -0,0 +1,17 @@
+
+import torch as tc
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, dropout):
+        super(MLP, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 1),
+        )
+    def forward(self, x):
+        return self.net(x)
