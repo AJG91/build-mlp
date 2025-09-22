a63879231dfe590d40bbea6dc11a31c8135b70a7
Alberto J Garcia <albertojg1991@gmail.com>
2025-09-22 12:07:22 -0700

    Added functions for data exploration such as correlation matrix and feature distribution plots

--- build_mlp/plots.py
+++ build_mlp/plots.py
@@ -1,10 +1,10 @@
-
+import numpy as np
+import pandas as pd
 import torch as tc
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from typing import Tuple
-
 
 def plot_pca(
     loader: tc.utils.data.DataLoader, 
@@ -32,7 +32,7 @@ def plot_pca(
         This is used to label the plot when saving to directory.
     iter_size : int, optional (default=100)
         Specifies the number of batch iterations to plot.
-    figsize : Tuple, optional (default=[10, 8])
+    figsize : Tuple, optional (default=(10, 8))
         Specifies the figure size.
     """
     X_batch, y_batch = [], []
@@ -53,7 +53,7 @@ def plot_pca(
     fig = plt.figure(figsize=figsize)
     
     if n_components == 3:
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111, projection="3d")
         ax.scatter(X_pca[..., 0], X_pca[..., 1], X_pca[..., 2], c=y_batch, cmap="coolwarm", alpha=0.5)
         ax.set_zlabel("PC 3", fontsize=16)
     else:
@@ -67,7 +67,7 @@ def plot_pca(
     plt.show();
     
     fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
-    fig.savefig(path + f'pca_{data}_{iter_size}_batches.png', bbox_inches='tight', dpi=dpi)
+    fig.savefig(path + f"pca_{data}_{iter_size}_batches.png", bbox_inches="tight", dpi=dpi)
 
 def plot_t_sne(
     loader: tc.utils.data.DataLoader, 
@@ -95,7 +95,7 @@ def plot_t_sne(
         This is used to label the plot when saving to directory.
     iter_size : int, optional (default=100)
         Specifies the number of batch iterations to plot.
-    figsize : Tuple, optional (default=[10, 8])
+    figsize : Tuple, optional (default=(10, 8))
         Specifies the figure size.
     """    
     X_batch, y_batch = [], []
@@ -116,12 +116,12 @@ def plot_t_sne(
     fig = plt.figure(figsize=figsize)
     
     if n_components == 3:
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(X_tsne[..., 0], X_tsne[..., 1], X_tsne[..., 2], c=y_batch, cmap='coolwarm', alpha=0.5)
+        ax = fig.add_subplot(111, projection="3d")
+        ax.scatter(X_tsne[..., 0], X_tsne[..., 1], X_tsne[..., 2], c=y_batch, cmap="coolwarm", alpha=0.5)
         ax.set_zlabel("t-SNE 3", fontsize=16)
     else:
         ax = fig.subplots()
-        ax.scatter(X_tsne[..., 0], X_tsne[..., 1], c=y_batch, cmap='coolwarm', alpha=0.5)
+        ax.scatter(X_tsne[..., 0], X_tsne[..., 1], c=y_batch, cmap="coolwarm", alpha=0.5)
 
     ax.set_title(f"t-SNE projection ({iter_size} batches)", fontsize=18)
     ax.set_xlabel("t-SNE 1", fontsize=16)
@@ -130,10 +130,198 @@ def plot_t_sne(
     plt.show();
     
     fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
-    fig.savefig(path + f't_sne_{data}_{iter_size}_batches.png', bbox_inches='tight', dpi=dpi)
+    fig.savefig(path + f"t_sne_{data}_{iter_size}_batches.png", bbox_inches="tight", dpi=dpi)
 
 
+def plot_feature_distributions(
+    df: pd.DataFrame, 
+    path: str, 
+    dpi: int, 
+    data: str,
+    bins: int = 30, 
+    figsize: Tuple = (15, 10)
+) -> None:
+    """
+    Plots histograms of the different features.
+    Outputs a 3x3 plot.
 
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the dataset
+    path : str
+        String to the directory where the plot is saved.
+    dpi : int
+        Dots per inch.
+        A higher dpi results in a sharper image.
+    data : str
+        The name of the dataset being plotted.
+        This is used to label the plot when saving to directory.
+    bins : int, optional (default=30)
+        Specifies the number of bins in distribution.
+    figsize : Tuple, optional (default=(15, 10))
+        Specifies the figure size.
+    """
+    fig, axes = plt.subplots(
+        nrows=3, ncols=3, figsize=figsize, constrained_layout=True
+    )
+    df.hist(bins=bins, ax=axes, grid=False)
+    for ax in axes.ravel():
+        ax.set_title(ax.get_title(), fontsize=14)
+        
+    plt.show();
+    
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
+    fig.savefig(path + f"feature_distribution_{data}.png", bbox_inches="tight", dpi=dpi)
+
+def plot_price_by_location(
+    df: pd.DataFrame, 
+    path: str, 
+    dpi: int, 
+    data: str,
+    figsize: Tuple = (10, 8)
+) -> None:
+    """
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the dataset
+    path : str
+        String to the directory where the plot is saved.
+    dpi : int
+        Dots per inch.
+        A higher dpi results in a sharper image.
+    data : str
+        The name of the dataset being plotted.
+        This is used to label the plot when saving to directory.
+    figsize : Tuple, optional (default=(10, 8))
+        Specifies the figure size.
+    """
+    fig = plt.figure(figsize=figsize)
+    
+    sc = plt.scatter(
+        df["Longitude"], df["Latitude"], c=df["MedHouseVal"],
+        cmap="viridis", s=20, alpha=0.5
+    )
+    cbar = plt.colorbar(sc)
+    cbar.set_label("Median House Value", fontsize=14)
+    cbar.ax.tick_params(labelsize=14)
+    plt.xlabel("Longitude", fontsize=14)
+    plt.ylabel("Latitude", fontsize=14)
+    plt.show();
+    
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
+    fig.savefig(path + f"price_by_location_{data}.png", bbox_inches="tight", dpi=dpi)
+
+def plot_population_and_prices(
+    df: pd.DataFrame, 
+    path: str, 
+    dpi: int, 
+    data: str,
+    figsize: Tuple = (10, 8)
+) -> None:
+    """
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the dataset
+    path : str
+        String to the directory where the plot is saved.
+    dpi : int
+        Dots per inch.
+        A higher dpi results in a sharper image.
+    data : str
+        The name of the dataset being plotted.
+        This is used to label the plot when saving to directory.
+    figsize : Tuple, optional (default=(10, 8))
+        Specifies the figure size.
+    """
+    fig = plt.figure(figsize=figsize)
+    
+    sc = plt.scatter(
+        df["Longitude"], df["Latitude"], 
+        alpha=0.4, s=df["Population"]/100,
+        c=df["MedHouseVal"], cmap="plasma"
+    )
+    cbar = plt.colorbar(sc)
+    cbar.set_label("Median House Value", fontsize=14)
+    cbar.ax.tick_params(labelsize=14)
+    plt.xlabel("Longitude", fontsize=14)
+    plt.ylabel("Latitude", fontsize=14)
+    plt.show();
+    
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
+    fig.savefig(path + f"price_and_population_{data}.png", bbox_inches="tight", dpi=dpi)
+
+def plot_correlation_map(
+    df: pd.DataFrame, 
+    path: str, 
+    dpi: int, 
+    data: str,
+    figsize: Tuple = (10, 8)
+) -> None:
+    """
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the dataset
+    path : str
+        String to the directory where the plot is saved.
+    dpi : int
+        Dots per inch.
+        A higher dpi results in a sharper image.
+    data : str
+        The name of the dataset being plotted.
+        This is used to label the plot when saving to directory.
+    figsize : Tuple, optional (default=(10, 8))
+        Specifies the figure size.
+    """
+    corr = df.corr()
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(corr, cmap="coolwarm")
+    cbar = plt.colorbar(cax)
+    cbar.set_label("Correlation", fontsize=14)
+    
+    for (i, j), val in np.ndenumerate(corr.values):
+        ax.text(j, i, f"{val:.2f}", ha="center", va="center", color="black")
+    
+    ax.set_xticks(range(len(corr.columns)))
+    ax.set_yticks(range(len(corr.columns)))
+    ax.set_xticklabels(corr.columns, rotation=90)
+    ax.set_yticklabels(corr.columns)
+    plt.show();
+    
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
+    fig.savefig(path + f"correlation_map_{data}.png", bbox_inches="tight", dpi=dpi)
+
+def plot_metrics_vs_epochs(
+    train_metric: np.ndarray, 
+    val_metric: np.ndarray, 
+    epochs: int, 
+    path: str, 
+    dpi: int, 
+    metric: str, 
+    figsize: Tuple = (10, 8)
+) -> None:
+    """
+    """
+    epochs_mesh = tc.arange(0, epochs)
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(epochs_mesh, train_metric, label="train set")
+    ax.plot(epochs_mesh, val_metric, label="validation set")
+
+    ax.set_xlabel("epochs", fontsize=14)
+    ax.set_ylabel(f"{metric}", fontsize=14)
+    ax.legend(fontsize=14)
+    plt.show();
+    
+    fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.01)
+    fig.savefig(path + f"{metric}_vs_epochs.png", bbox_inches="tight", dpi=dpi)
 
     
 
